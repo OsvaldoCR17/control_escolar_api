3a4c45e9a79a7e81bfe48a78813f9f22f270bd8c
Osvaldo Cornelio de la Rosa <osvaldocr343@gmail.com>
2025-12-03 15:31:44 -0600

    duplicado

--- control_escolar_api/settings.py
+++ control_escolar_api/settings.py
@@ -30,7 +30,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',     # CORS debe ir antes de CommonMiddleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
