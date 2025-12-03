985bde7cb42680d4a5bec6b745fa9c6e04f8b74f
Osvaldo Cornelio de la Rosa <osvaldocr343@gmail.com>
2025-12-03 13:53:42 -0600

    cambios

--- control_escolar_api/settings.py
+++ control_escolar_api/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = '-_&+lsebec(whhw!%n@ww&1j=4-^j_if9x8$q778+99oz&!ms2'
 
 DEBUG = True  # en desarrollo
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
